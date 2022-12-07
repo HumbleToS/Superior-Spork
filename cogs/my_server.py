094ba46cfa1e1f00ed90fd359f68354a839c2d6e
Humble <70359789+HumbleToS@users.noreply.github.com>
2022-12-07 16:58:12 -0700

    Update very old naming

--- cogs/my_server.py
+++ cogs/my_server.py
@@ -11,22 +11,22 @@ class MyServer(commands.Cog, name="ass"):
         if member.guild.id != 953535172941324298:
             return
         if member.bot:
-            bot_adds = self.bot.get_channel(958068139986411581)
-            bot_adds_em = discord.Embed(
+            bot_log = self.bot.get_channel(958068139986411581)
+            bot_log_embed = discord.Embed(
                 title="Discord Bot Added",
                 description=f"{member.name}#{member.discriminator} `({member.id})`",
                 color=0xF87676,
             )
-            return await bot_adds.send(f"{member.guild.owner.mention}", embed=bot_adds_em)
-        channel = self.bot.get_channel(953535172941324301)
-        luversRole = discord.utils.get(member.guild.roles, name="Luvers")
-        await member.add_roles(luversRole)
-        joinEmbed = discord.Embed(
+            return await bot_log.send(f"{member.guild.owner.mention}", embed=bot_log_embed)
+        welcome_channel = self.bot.get_channel(953535172941324301)
+        luvers_role = discord.utils.get(member.guild.roles, name="Luvers")
+        await member.add_roles(luvers_role)
+        join_embed = discord.Embed(
             description=f"Read {self.bot.get_channel(953743551852851271).mention} for information & rules!\nCheck out [Roles] & [Colors] for more <3",
             color=0xF87676,
         )
-        joinEmbed.set_author(name="𝙰 𝚂 𝚂", icon_url=member.guild.icon.url)
-        await channel.send(member.mention, embed=joinEmbed, delete_after=15)
+        join_embed.set_author(name="𝙰 𝚂 𝚂", icon_url=member.guild.icon.url)
+        await welcome_channel.send(member.mention, embed=join_embed, delete_after=15)
 
 
 async def setup(bot):
