93a24fc07fc2ff3090f7edf44fcb5060629f3705
Humble <70359789+HumbleToS@users.noreply.github.com>
2022-12-10 17:23:07 -0700

    Remove admin, owner and eval checks Along with removing things regarding config And hard coded lists for personal cogs/commands

--- utils/checks.py
+++ utils/checks.py
@@ -1,55 +1,8 @@
-import json
-
 from discord.ext import commands
-
-config = json.load(open(r"././config.json"))
-
-default_prefix = config.get('default_prefix')
-admins = config.get('admins')
-owner = config.get('owner')
-beta = config.get('beta')
-
-rocky_cmds = [613202332880207872, 896130578221506610, 780417367548756000, 739219467455823921]
-eval_users = [739219467455823921, 780417367548756000, 353340381359767552, 524016475661664256]
 
 
 class NotGuildOwner(commands.CheckFailure):
     pass
-
-
-class NotBotAdmin(commands.CheckFailure):
-    pass
-
-
-def is_nightmare():
-    async def is_nightmare_check(ctx):
-        return ctx.guild.id == 812691071867944971
-
-    return commands.check(is_nightmare_check)
-
-
-def is_dick_users():
-    async def is_rocky_user_check(ctx):
-        return ctx.author.id in rocky_cmds
-
-    return commands.check(is_rocky_user_check)
-
-
-def is_admin():
-    async def is_admin_check(ctx):
-        if ctx.author.id in admins + owner:
-            return True
-        else:
-            raise NotBotAdmin
-
-    return commands.check(is_admin_check)
-
-
-def is_real_owner():
-    async def is_real_owner_check(ctx):
-        return ctx.author.id in owner
-
-    return commands.check(is_real_owner_check)
 
 
 def is_guild_owner():
@@ -60,10 +13,3 @@ def is_guild_owner():
             raise NotGuildOwner
 
     return commands.check(guild_owner)
-
-
-def eval_perms():
-    async def eval_perms_check(ctx):
-        return ctx.author.id in eval_users
-
-    return commands.check(eval_perms_check)
